11205eb209feb191b19f2df287e7b2ab7f5d21b4
Ben Civjan <bcivjan2@illinois.edu>
2020-06-05 23:43:15 -0700

    Added logic to rendered grid

--- public/render.js
+++ public/render.js
@@ -1,18 +1,22 @@
+/** Canvas */
 let canvas;
+/** Canvas context */
 let c;
 let mouse = {
 	x: undefined,
 	y: undefined
 };
+let cells = 10;
+let gridVals = [];
 
-class Node {
-	constructor(isWall, xCoord, yCoord, displayVal, g = Number.POSITIVE_INFINITY, h = Number.POSITIVE_INFINITY) {
+class GridNode {
+	constructor(isWall, xCoord, yCoord, path, g = Number.POSITIVE_INFINITY, h = Number.POSITIVE_INFINITY) {
 		this.isWall = isWall;
 		this._xCoord = xCoord;
 		this._yCoord = yCoord;
+		this.path = path;
 		this.g = g;
 		this.h = h;
-		this.displayVal = displayVal;
 	}
 	get xCoord() {
 		return this._xCoord;
@@ -25,32 +29,75 @@ class Node {
 	}
 }
 
+//==================
+//RUNNING CODE HERE
+//==================
+
 init();
 
-function init() {
-	canvas = document.querySelector("#canvas");
-	console.log(canvas);
-	console.log(this);
-
-	c = canvas.getContext("2d");
-	draw();
-}
-
+// MOUSE MOVE LISTENER
 window.addEventListener("mousemove", function(event) {
 	mouse.x = event.x;
 	mouse.y = event.y;
 });
-window.addEventListener("click", event => {
-	console.log(mouse.x, mouse.y);
+
+// MOUSE CLICK LISTENER
+canvas.addEventListener("click", event => {
+	// Loop through all nodes to check if click event happened in it
+	outer: for (let x in gridVals) {
+		for (let y in gridVals[x]) {
+			let node = gridVals[x][y];
+			// Check if click happened in this node
+			if (c.isPointInPath(node.path, event.offsetX, event.offsetY)) {
+				console.log(node.xCoord, node.yCoord);
+
+				//Break search if we found target
+				break outer;
+			}
+		}
+	}
 });
 
-function draw() {
-	// const height = canvas.height;
-	// const width = canvas.width;
+// INITIALIZATION FUNCTION
+function init() {
+	canvas = document.querySelector("#canvas");
+	c = canvas.getContext("2d");
 
-	c.fillStyle = "rgb(200, 0, 0)";
-	c.fillRect(10, 10, 50, 50);
+	initGrid(cells);
+}
 
-	c.fillStyle = "rgba(0, 0, 200, 0.5)";
-	c.fillRect(0, 0, 50, 50);
+// INITIALIZE GRID
+function initGrid(cellNum) {
+	const height = canvas.height;
+	const width = canvas.width;
+
+	c.strokeStyle = "gray";
+	c.lineWidth = 2;
+	c.fillStyle = "#eeeeee";
+
+	let cellWidth = width / cellNum;
+	let cellHeight = height / cellNum;
+
+	for (let x = 0; x < cellNum; x++) {
+		// Create new nested array
+		gridVals.push(new Array(cellNum));
+
+		for (let y = 0; y < cellNum; y++) {
+			let node = new GridNode(false, x, y);
+
+			// Create rectangle path and store in GridNode object
+			node.path = new Path2D();
+			node.path.rect(cellWidth * x, cellHeight * y, cellWidth, cellHeight);
+
+			// Draw GridNode on the canvas
+			c.fill(node.path);
+			c.stroke(node.path);
+			// Store the node in the grid array
+			gridVals[x][y] = node;
+		}
+	}
+}
+
+function animate() {
+	requestAnimationFrame(animate);
 }
