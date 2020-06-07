8d25c102a30566b25118685efa0ae77b276cf4eb
Ben Civjan <bcivjan2@illinois.edu>
2020-06-06 19:27:18 -0700

    Added buttons and clicking functionality

--- scripts/astar.js
+++ scripts/astar.js
@@ -1,52 +1,3 @@
-const _ = require("underscore");
-
-class Node {
-	constructor(isWall, xCoord, yCoord, displayVal, g = Number.POSITIVE_INFINITY, h = Number.POSITIVE_INFINITY) {
-		this.isWall = isWall;
-		this._xCoord = xCoord;
-		this._yCoord = yCoord;
-		this.g = g;
-		this.h = h;
-		this.displayVal = displayVal;
-	}
-	get xCoord() {
-		return this._xCoord;
-	}
-	get yCoord() {
-		return this._yCoord;
-	}
-	get f() {
-		return this.g + this.h;
-	}
-}
-
-// ALGORITHM
-
-let testGrid = [ [], [], [], [], [], [], [], [], [], [] ];
-
-for (let x = 0; x < 10; x++) {
-	for (let y = 0; y < 10; y++) {
-		testGrid[x][y] = new Node(false, x, y, "   ");
-	}
-}
-
-let start = testGrid[0][6];
-let end = testGrid[9][9];
-
-//Wall tests
-testGrid[5][9].isWall = true;
-testGrid[5][8].isWall = true;
-testGrid[5][7].isWall = true;
-testGrid[5][6].isWall = true;
-// testGrid[5][5].isWall = true;
-testGrid[5][4].isWall = true;
-
-let last = astar(testGrid, start, end);
-
-pathTrace(last);
-
-printGrid(testGrid);
-
 function astar(grid, start, end) {
 	let open = [];
 	let closed = [];
@@ -61,6 +12,7 @@ function astar(grid, start, end) {
 
 	while (open.length > 0) {
 		// current is node with smallest f cost in open list
+		// eslint-disable-next-line no-undef
 		current = _.min(open, node => node.f);
 		// Remove current from the open list
 		open.splice(open.indexOf(current), 1);
@@ -101,7 +53,9 @@ function distance(current, destination) {
 	let xDist = Math.abs(current.xCoord - destination.xCoord);
 	let yDist = Math.abs(current.yCoord - destination.yCoord);
 
+	// eslint-disable-next-line no-undef
 	let diagonal = _.min([ xDist, yDist ]);
+	// eslint-disable-next-line no-undef
 	let straight = _.max([ xDist, yDist ]) - diagonal;
 	// Distance using manhattan and diagonal routes
 	let dist = Math.sqrt(2) * diagonal + straight;
@@ -134,15 +88,4 @@ function pathTrace(last) {
 		next = next.parent;
 	}
 	last.displayVal = "End";
-}
-
-function printGrid(grid) {
-	let printableGrid = [ [], [], [], [], [], [], [], [], [], [] ];
-	for (let x = 0; x < 10; x++) {
-		for (let y = 0; y < 10; y++) {
-			printableGrid[x][y] = grid[x][y].displayVal;
-		}
-	}
-
-	console.table(printableGrid);
 }
--- scripts/render.js
+++ scripts/render.js
@@ -2,12 +2,17 @@
 let canvas;
 /** Canvas context */
 let c;
+let runBtn;
+let clearBtn;
+
 let mouse = {
 	x: undefined,
 	y: undefined
 };
-let cells = 10;
+let cells = 20;
 let gridVals = [];
+let startNode;
+let endNode;
 
 class GridNode {
 	constructor(isWall, xCoord, yCoord, path, g = Number.POSITIVE_INFINITY, h = Number.POSITIVE_INFINITY) {
@@ -35,33 +40,44 @@ class GridNode {
 
 init();
 
-// MOUSE MOVE LISTENER
-window.addEventListener("mousemove", function(event) {
-	mouse.x = event.x;
-	mouse.y = event.y;
-});
-
-// MOUSE CLICK LISTENER
-canvas.addEventListener("click", event => {
-	// Loop through all nodes to check if click event happened in it
-	outer: for (let x in gridVals) {
-		for (let y in gridVals[x]) {
-			let node = gridVals[x][y];
-			// Check if click happened in this node
-			if (c.isPointInPath(node.path, event.offsetX, event.offsetY)) {
-				console.log(node.xCoord, node.yCoord);
-
-				//Break search if we found target
-				break outer;
-			}
-		}
-	}
-});
-
 // INITIALIZATION FUNCTION
 function init() {
 	canvas = document.querySelector("#canvas");
 	c = canvas.getContext("2d");
+	runBtn = document.querySelector("#run");
+	clearBtn = document.querySelector("#clear");
+
+	// MOUSE MOVE LISTENER
+	window.addEventListener("mousemove", function(event) {
+		mouse.x = event.x;
+		mouse.y = event.y;
+	});
+
+	// MOUSE CLICK LISTENER
+	canvas.addEventListener("click", event => {
+		// TODO: Improve searching
+
+		// Loop through all nodes to check if click event happened in it
+		outer: for (let x in gridVals) {
+			for (let y in gridVals[x]) {
+				let node = gridVals[x][y];
+				// Check if click happened in this node
+				if (c.isPointInPath(node.path, event.offsetX, event.offsetY)) {
+					node.isWall = !node.isWall;
+					c.clearRect(0, 0, canvas.width, canvas.height);
+					renderGrid();
+					//Break search if we found target
+					break outer;
+				}
+			}
+		}
+	});
+
+	runBtn.addEventListener("click", () => {
+		// TODO: Run a* algorithm here
+	});
+
+	clearBtn.addEventListener("click", () => initGrid(cells));
 
 	initGrid(cells);
 }
@@ -94,6 +110,23 @@ function initGrid(cellNum) {
 			c.stroke(node.path);
 			// Store the node in the grid array
 			gridVals[x][y] = node;
+		}
+	}
+}
+
+function renderGrid() {
+	for (let x in gridVals) {
+		for (let y in gridVals[x]) {
+			let node = gridVals[x][y];
+			c.strokeStyle = "gray";
+			c.lineWidth = 2;
+			c.fillStyle = "#eeeeee";
+			if (node.isWall) c.fillStyle = "lightblue";
+			if (node.isPath) c.fillStyle = "lightyellow";
+			if (node.isStart) c.fillStyle = "blue";
+			if (node.isEnd) c.fillStyle = "red";
+			c.fill(node.path);
+			c.stroke(node.path);
 		}
 	}
 }
