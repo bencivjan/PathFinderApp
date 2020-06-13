85d8ef3effd914d2cfe72505e502535bd6c856d7
Ben Civjan <bcivjan2@illinois.edu>
2020-06-13 13:29:34 -0700

    Bug fixes

--- scripts/astar.js
+++ scripts/astar.js
@@ -38,9 +38,9 @@ async function astar(grid, start, end) {
 				continue;
 			}
 			// If path to neighbor (g cost) is shorter than current
-			if (distance(start, current) + distance(current, neighbor) < neighbor.g) {
+			if (current.g + distance(current, neighbor) < neighbor.g) {
 				// Set f cost (through g and h)
-				neighbor.g = distance(start, current) + distance(current, neighbor);
+				neighbor.g = current.g + distance(current, neighbor);
 				neighbor.h = distance(neighbor, end);
 				// Set parent of neighbor to current
 				neighbor.parent = current;
